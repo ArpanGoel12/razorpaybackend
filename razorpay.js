cc677ea946dba73e0a6dd2488ad23f2e28e5aee6
ArpanGoel12 <117060920+ArpanGoel12@users.noreply.github.com>
2023-05-28 16:21:54 +0530

    Update razorpay.js

--- razorpay.js
+++ razorpay.js
@@ -11,7 +11,7 @@ var instance = new Razorpay({
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
